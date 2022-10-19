38e3b4ff095c4c7f67ea42879479b76f7586930e
ThahaShahzad <61521512+ThahaShahzad@users.noreply.github.com>
2022-10-19 15:18:39 -0500

    Init Nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>League Team Comp Generator</title>
+        <meta name='description' content='League Team Comp Generator' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <h2>League Team Comp Generator</h2>
+      </main>
+    </div>
+  )
+}
+
+export default Home
